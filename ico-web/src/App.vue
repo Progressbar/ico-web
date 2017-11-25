75bf42a8ef846d09f67eca66bdfc636de6b1088f
Matej Nemček <ybdaba@gmail.com>
2017-11-25 14:37:04 +0100

    init vue pwa

---
+++ ico-web/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <header>
+      <span>ico.progressbar hackerspace</span>
+    </header>
+    <main>
+      <img src="./assets/progressbar_negative.png" alt="progressbar hackerspace">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #000000;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
---
+++ ico-web/src/components/Hello.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>~ ~</h2>
+    <ul>
+      <li>
+        ico
+      </li>
+      <li>
+        why
+      </li>
+      <li>
+        when
+      </li>
+      <li>
+        hardcap?
+      </li>
+    </ul>
+    <h2>Why you want help us</h2>
+    <ul>
+      <li>
+        ~
+      </li>
+      <li>
+        ~
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'ico.progressbar.sk'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
